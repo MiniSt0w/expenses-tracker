15f8dd759cb60fe7486e9677fedd05b6193226cd
OmarAbouSerieh <omar@global.com>
2025-08-11 14:48:25 +0300

    changing dto

--- src/main/java/com/training/expenseTracker/dto/categories/CategoriesReadDTO.java
+++ src/main/java/com/training/expenseTracker/dto/categories/CategoriesReadDTO.java
@@ -1,11 +1,8 @@
 package com.training.expenseTracker.dto.categories;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter @AllArgsConstructor
-public class CategoriesReadDTO {
-    private int categoryId;
-    private String categoryName;
-    private String categoryColor;
+public record CategoriesReadDTO (
+     int categoryId,
+     String categoryName,
+     String categoryColor
+){
 }
